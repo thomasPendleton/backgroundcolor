8f6bb6456035e0bee74526027aae1a95ad97a3c1
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2020-08-22 23:07:34 -0700

    change styling

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const css = document.querySelector("h4")
+const css = document.querySelector("h4");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.querySelector("body");
 
-function setGradient(){
-    // console.log(color1.value)
-    // console.log(color2.value)
-    body.style.background = "linear-gradient(to right, "
-    + color1.value 
-    + " , " 
-    + color2.value 
-    + ")";
-    css.textContent = body.style.background
-};
+function setGradient() {
+  console.log(color1.value)
+  console.log(color2.value)
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
+  css.innerHTML = `<h4>Linear-Gradient
+        <br>Left: ${color1.value} to right: ${color2.value}</h4>
+  `
+  
+  
+//   body.style.background;
+}
 
 color1.addEventListener("input", setGradient);
 
