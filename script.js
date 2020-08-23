3249b1658356e950d0812ced086938b913b4510a
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2020-08-22 21:24:08 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const css = document.querySelector("h4")
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.querySelector("body");
+
+function setGradient(){
+    // console.log(color1.value)
+    // console.log(color2.value)
+    body.style.background = "linear-gradient(to right, "
+    + color1.value 
+    + " , " 
+    + color2.value 
+    + ")";
+    css.textContent = body.style.background
+};
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+//linear-gradient(to right, rgb(130, 132, 123), rgb(153, 77, 153))
